b6edf46d7510132cfd60caf6a5ad53838e77bdf2
diveMatrix_NoteBook <97281082+divematrix@users.noreply.github.com>
2022-01-29 16:39:37 +0900

    ex05_python01

--- ex02/python/lengthStr_1_01.py
+++
@@ -1,7 +0,0 @@
-str = input("문자열을 입력해 주세요")
-
-# 문자열 포매팅 방법 1
-# print("What is the input string? %s\n%s has %d characters." % (str, str, len(str)))
-
-# 문자열 포매팅 방법 2 - https://blockdmask.tistory.com/424
-print("What is the input string? {0}\n{0} has {1} characters.".format(str, len(str)))
--- ex04/ex04_python01.py
+++ ex04/ex04_python01.py
@@ -6,4 +6,5 @@ adjective = input("Enter a adjective : ")
 
 adverb = input("Enter a adverb : ")
 
+# 문자열 포맷팅을 이용하여 출력문 구성
 print("Do you {1} your {2} {0} {3}? That`s hilarious!".format(noun, verb, adjective, adverb))
