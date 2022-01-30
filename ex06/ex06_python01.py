dd722ad5c5eb00aa083cec14402f37ea767cfd3b
diveMatrix_NoteBook <97281082+divematrix@users.noreply.github.com>
2022-01-30 12:10:42 +0900

    ex06 python01

---
+++ ex06/ex06_python01.py
@@ -0,0 +1,9 @@
+a = int(input("What is your current age? "))
+
+b = int(input("At what age would you like to retire? "))
+
+import datetime
+
+current = datetime.datetime.now()
+
+print("you have {} years left until you can retire. It`s {}, so you can retire in {}.".format(b-a, current.year, current.year + b - a))
