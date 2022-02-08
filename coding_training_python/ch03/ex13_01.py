5badc3954fe3ef0bf60be7ef5be0af0048e56528
DiveMatrix_DeskTop <97281082+divematrix@users.noreply.github.com>
2022-02-08 22:00:42 +0900

    코딩 트레이닝 연습문제 13 풀이

--- coding_training_python/ch03/ex13_01.py
+++ coding_training_python/ch03/ex13_01.py
@@ -1,1 +1,15 @@
 # 연습문제 13. Compound Interest
+
+principal = float(input("What is the principal amount? "))
+
+interest_Rate_annual_percent = float(input("What is the rate: "))
+
+interest_Rate_annual = interest_Rate_annual_percent / 100
+
+term_annual = float(input("What is the number of years: "))
+
+interest_times_per_year = float(input("What is the number of times the interest is compounded per year: "))
+
+principal_and_interest = principal * (1 + interest_Rate_annual / interest_times_per_year) ** (interest_times_per_year * term_annual)
+
+print("${:.0f} invested at {}% for {:.0f} years compounded {:.0f} times per year is ${:.2f}".format(principal, interest_Rate_annual_percent, term_annual, interest_times_per_year, principal_and_interest))
