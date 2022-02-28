9ffd145d218d17c6d47dbad409eabfbfd8b7569d
DiveMatrix_DeskTop <97281082+divematrix@users.noreply.github.com>
2022-02-28 19:28:49 +0900

    코딩 트레이닝 연습문제 36

--- coding_training/python_practice/ch07/ex36_01.py
+++ coding_training/python_practice/ch07/ex36_01.py
@@ -1,5 +1,30 @@
 # 연습문제 36. 통계 계산
 
+def sum(list):
+	result = 0
+	for i in list:
+		result += i
+	return result
+
+def average(list):
+	return sum(list) / len(list)
+
+def deviation(list):
+	result = []
+	for i in list:
+		result.append(i - average(list))
+	return result
+
+def variance(list):
+	result = 0
+	for i in deviation(list):
+		result += i ** 2
+	return result / len(list)
+
+def stddev(list):
+	return variance(list) ** (1/2)
+
+
 time = []
 
 while True:
@@ -11,5 +36,17 @@ while True:
 	if num == "done":
 		break
 
+print("Numbers : ", end="")
+
 for i in time:
-	
+	print("{} ".format(i), end="")
+
+print("")
+
+print("The average is {}.".format(average(time)))
+
+print("The minimum is {}.".format(min(time)))
+
+print("The maximum is {}.".format(max(time)))
+
+print("The standard deviation is {}.".format(stddev(time)))
