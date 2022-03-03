24641c73b1ed47531ce7c4799c45991aac94c591
DiveMatrix_DeskTop <97281082+divematrix@users.noreply.github.com>
2022-03-03 19:06:36 +0900

    코딩 트레이닝 연습문제 40

--- coding_training/python_practice/ch07/ex40_01.py
+++ coding_training/python_practice/ch07/ex40_01.py
@@ -1,16 +1,21 @@
-# 연습문제 39. 레코드 정렬
+# 연습문제 40. 필터링 레코드
 
 foo = [{'First Name':'John', 'Last Name': 'Johnson', 'Position': 'Manager', 'Separation date': '2016-12-31'}, {'First Name':'Tou', 'Last Name': 'Xiong', 'Position': 'Software Engineer', 'Separation date': '2016-10-15'}, {'First Name':'Michaela', 'Last Name': 'Michaelson', 'Position': 'District Manager', 'Separation date': '2015-12-19'}, {'First Name':'Jake', 'Last Name': 'Jacobson', 'Position': 'Programmer', 'Separation date': ''}, {'First Name':'Jacquelyn', 'Last Name': 'Jackson', 'Position': 'DBA', 'Separation date': ''}, {'First Name':'Sally', 'Last Name': 'Weber', 'Position': 'Web Developer', 'Separation date': '2015-12-18'}]
 
-for j in range(len(foo) - 1, 0, -1):
-	for i in range(0, j):
-		if foo[i]['Last Name'] > foo[i+1]['Last Name']:
-			foo[i], foo[i+1] = foo[i+1], foo[i]
+srch = input("Enter a search string : ")
 
-print("Name                | Position          | Separation Date")
-
-print("--------------------|-------------------|--------------------")
+result = []
 
 for i in foo:
+	if srch in i['First Name']:
+		result.append(i)
+	elif srch in i['Last Name']:
+		result.append(i)
+
+print("\nResult : ")
+print("Name                | Position          | Separation Date")
+print("--------------------|-------------------|------------------")
+
+for i in result:
 	name = i['First Name'] + " " + i['Last Name']
 	print("{:<20}| {:<17} | {}".format(name, i['Position'], i['Separation date']))
